bf5c7637f5c3a6699accbbaecefb19b6bf0c7204
Antonio Ragusa <antonioragusa1253@gmail.com>
2022-03-14 18:38:42 -0400

    Created functionality for the EmployeeDao and ProductDao classes

--- src/main/java/com/revature/dao/ProductDao.java
+++ src/main/java/com/revature/dao/ProductDao.java
@@ -1,5 +1,84 @@
 package com.revature.dao;
 
-public class ProductDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
+import org.apache.log4j.Logger;
+
+import com.revature.models.Product;
+import com.revature.util.ColumnField;
+import com.revature.util.Configuration;
+import com.revature.util.ConnectionUtil;
+import com.revature.util.MetaModel;
+
+public class ProductDao {
+	
+	private static Logger logger = Logger.getLogger(ProductDao.class);
+	List<Class<?>> tableClass = Arrays.asList(Product.class);
+
+	static Configuration cfg = new Configuration();
+	
+public boolean createTable() {
+		
+		cfg.addAnnotatedClasses(tableClass);
+		
+		try(Connection conn = ConnectionUtil.getConnection()) {
+			
+			for (MetaModel<?> metaModel : cfg.getMetaModels()) {
+				String sql = "CREATE TABLE IF NOT EXISTS " + metaModel.getEntity(/*products*/) + " (\r\n"
+					+ "			" + metaModel.getPrimaryKey().getColumnName(/*product_id*/) +  " SERIAL PRIMARY KEY\r\n"
+					+ ");			";
+				
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				
+				stmt.executeUpdate();
+				logger.info("The " + metaModel.getEntity() + " table has been created!");
+				
+				List<ColumnField> columns = metaModel.getColumns();
+				
+				try(Connection conn2 = ConnectionUtil.getConnection()) {
+					
+					for (ColumnField cf : columns) {
+						
+						String datatype = "";
+						
+						if (cf.getType().toString().equals("class java.lang.String")) {
+							datatype = "VARCHAR(50)";
+						}
+						
+						if (cf.getType().toString().equals("double")) {
+							datatype = "NUMERIC(50,2)";
+						}
+						
+						if (cf.getType().toString().equals("int")) {
+							datatype = "INTEGER";
+						}
+						
+						String sql2 = "ALTER TABLE " + metaModel.getEntity() + "\r\n"
+									+    "ADD COLUMN IF NOT EXISTS " + cf.getColumnName() + " " + datatype + ";";
+						
+					PreparedStatement stmt2 = conn.prepareStatement(sql2);
+					
+					stmt2.executeUpdate();
+					logger.info("The " + cf.getColumnName() +" table has been created!");	
+						
+					}
+					
+				} catch (SQLException e) {
+					logger.warn("Unable to create " + metaModel.getSimpleClassName() + " column in the DB");
+					e.printStackTrace();
+				}
+				
+			}
+			return true;
+		} catch (SQLException e) {
+			logger.warn("Unable to create Product table in the DB");
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 }
--- src/main/java/com/revature/main/Driver.java
+++ src/main/java/com/revature/main/Driver.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.revature.dao.CustomerDao;
+import com.revature.dao.EmployeeDao;
+import com.revature.dao.ProductDao;
 import com.revature.models.Customer;
 import com.revature.models.Employee;
 import com.revature.models.Product;
 import com.revature.util.ColumnField;
 import com.revature.util.Configuration;
 import com.revature.util.ConnectionUtil;
-import com.revature.util.EntityField;
 import com.revature.util.MetaModel;
 import com.revature.util.PrimaryKeyField;
 
@@ -23,6 +24,8 @@ public class Driver {
 		
 		Configuration cfg = new Configuration();
 		CustomerDao customerDao = new CustomerDao();
+		EmployeeDao employeeDao = new EmployeeDao();
+		ProductDao productDao = new ProductDao();
 
 		//cfg.addAnnotatedClasses(myClasses);
 		ConnectionUtil.getConnection();
@@ -35,7 +38,7 @@ public class Driver {
 for (MetaModel<?> metaModel : cfg.getMetaModels()) {
 			
 			// for each one, print the information about the metaModel
-			System.out.println("Printing all the MetaModel info for the class: " + metaModel.getClassName());
+			System.out.println("Printing all the MetaModel info for the class: " + metaModel.getSimpleClassName());
 			System.out.println("Trying to get Entity name for Customer class: " + metaModel.getEntity());
 			PrimaryKeyField pk = metaModel.getPrimaryKey();
 			List<ColumnField> columns = metaModel.getColumns();
@@ -45,6 +48,8 @@ for (MetaModel<?> metaModel : cfg.getMetaModels()) {
 			
 			System.out.println("ATTEMPTING TO CREATE TABLE: ");
 			customerDao.createTable();
+			employeeDao.createTable();
+			productDao.createTable();
 			
 			for (ColumnField cf : columns) {
 				System.out.println("Found the column field named " + cf.getColumnName() + " with the type " + cf.getType());
